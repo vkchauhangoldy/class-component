ce29b0db928c3ccc494975db16c97a605b9b4830
VINAY KUMAR CHAUHAN <vkchauhan750@gmail.com>
2025-09-27 21:18:57 +0530

    production ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,38 +3,28 @@ import React, { Component } from "react";
 class Counter extends Component {
   constructor(props) {
     super(props);
-    // state holds the count
     this.state = {
       count: 0,
     };
   }
 
-  // increase count
   increment = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
-  // decrease count
   decrement = () => {
     this.setState({ count: this.state.count - 1 });
-  };
-
-  // reset count
-  reset = () => {
-    this.setState({ count: 0 });
   };
 
   render() {
     return (
       <div className="container">
-        <h1>Counter App</h1>
+        {/* Display count in correct format */}
         <p>Count:{this.state.count}</p>
-        <button onClick={this.increment} style={{ margin: "5px" }}>
-          Increment
-        </button>
-        <button onClick={this.decrement} style={{ margin: "5px" }}>
-          Decrement
-        </button>
+
+        {/* Buttons must be named exactly "Increment" and "Decrement" */}
+        <button onClick={this.increment}>Increment</button>
+        <button onClick={this.decrement}>Decrement</button>
       </div>
     );
   }
